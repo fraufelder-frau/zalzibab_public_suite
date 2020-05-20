69397d16d207d1099f800e2acf14063e43920354
Zalzibab <zalzibab@protonmail.com>
2020-05-20 04:18:49 -0700

    Add existing file

---
+++ risk_management/MyBitmex.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from datetime import datetime, timedelta, timezone
+from sympy import symbols, Eq, solve
+from bybit import bybit
+from bitmex import bitmex
+from qtrade_client.api import QtradeAPI
+import requests
+import ast
+import os
+from os import walk
+import json
+import warnings
+warnings.simplefilter("ignore")
+import bravado.exception
+import io
+import time
+import logging
+import decimal
+import sys
+from MyFunctions import *
+
+
+def close_position(client, contract_to_view):
+    resp = client.Position.Position_get(filter = json.dumps({'isOpen': True, 'symbol': contract_to_view})).result()[0][0]
+    if resp['currentQty'] > 0:
+        client.Order.Order_new(symbol=contract_to_view, execInst='Close', side='Sell').result()
+    else:
+        client.Order.Order_new(symbol=contract_to_view, execInst='Close', side='Buy').result()
+    client.Order.Order_cancelAll(symbol=contract_to_view).result()
+    return print(contract_to_view+' Position Closed')
+
+
+def open_positions(client):
+    postions = []
+    resp = client.Position.Position_get(filter = json.dumps({'isOpen': True})).result()[0]
+    for x in range(len(resp)):
+        time.sleep(1)
+        current_bitmex = client.Position.Position_get(filter=json.dumps({'symbol': resp[x]['symbol']})).result()[0][0]    
+        open_orders = client.Order.Order_getOrders(symbol=resp[x]['symbol'], filter = json.dumps({'open': 'true'})).result()[0]
+        time.sleep(1)
+        if len(client.Order.Order_getOrders(symbol=resp[x]['symbol'], filter = json.dumps({'open': 'true', 'ordType': ['Limit', 'MarketIfTouched', 'StopLimit', 'LimitIfTouched']})).result()[0]) != 0:
+            close_order = client.Order.Order_getOrders(symbol = resp[x]['symbol'], filter = json.dumps({'open': 'true', 'ordType': ['Limit', 'MarketIfTouched', 'StopLimit', 'LimitIfTouched']})).result()[0]
+            close_price = usd_str(close_order[0]['price'])
+        else:
+            close_order = 'No Close Order Set'
+            close_price = 'No Close Order Set'
+        time.sleep(1)
+        if len(client.Order.Order_getOrders(symbol=resp[x]['symbol'], filter = json.dumps({'open': 'true', 'ordType': ['Stop', 'TrailingStop']})).result()[0]) > 0:
+            stop_order = client.Order.Order_getOrders(symbol=resp[x]['symbol'], filter = json.dumps({'open': 'true', 'ordType': ['Stop', 'TrailingStop']})).result()[0]
+            stop_price = usd_str(stop_order[0]['stopPx'])
+        else:
+            stop_order = 'NO STOP SET!!!'
+            stop_price = 'NO STOP SET!!!'
+        time.sleep(1)
+        mex = {}
+        mex['Contract'] = resp[x]['symbol']
+        if current_bitmex['currentQty'] < 0:
+            mex['Side'] = 'Short'
+        elif current_bitmex['currentQty'] > 0:
+            mex['Side'] = 'Long'
+        else:
+            mex['Side'] = 'None'
+        if mex['Side'] == 'Short':
+            mex['Size'] = current_bitmex['currentQty']*-1
+        else:
+            mex['Size'] = current_bitmex['currentQty']
+        mex['Entry'] = current_bitmex['avgEntryPrice']
+        mex['Target'] = close_price
+        mex['Stop'] = stop_price
+        mex['OpenValue'] = btc_float_round(mex['Size']*((1/mex['Entry'])-(1/mex['Entry'])*0.00075))
+        mex['MarketPrice'] = current_bitmex['markPrice']
+        mex['MarketValue'] = btc_float_round(mex['Size']*((1/mex['MarketPrice'])-(1/mex['MarketPrice'])*0.00075))
+        mex['Entry'] = usd_str(current_bitmex['avgEntryPrice'])
+        mex['MarketPrice'] = usd_str(current_bitmex['markPrice'])
+        if mex['Side'] == 'Long':
+            mex['UnrealisedPnL'] = btc_str(mex['OpenValue'] - mex['MarketValue'])
+        else:
+            mex['UnrealisedPnL'] = btc_str(mex['MarketValue'] - mex['OpenValue'])
+        postions.append(mex)
+    return postions
+
+
+def active_contracts(client):
+    contracts = requests.get('https://www.bitmex.com/api/v1/instrument/active').json()
+    contract_list = [x['symbol'] for x in bitmex_contracts]
+    return contract_list
+
+
+def new_trade(client, contract, size, entry, target, stop, order_type):
+    if order_type == 'Market':
+        client.Order.Order_cancelAll(symbol=contract).result()
+        client.Order.Order_new(symbol=contract, orderQty=size, ordType='Market').result()
+        client.Order.Order_new(symbol=contract, price=target, execInst='ReduceOnly', orderQty=(size*-1), ordType='Limit').result()
+        client.Order.Order_new(symbol=contract, stopPx=stop, execInst=str('LastPrice, ReduceOnly'), orderQty=(size*-1), ordType='Stop').result()
+    else:
+        client.Order.Order_cancelAll(symbol=contract).result()
+        client.Order.Order_new(symbol=contract, orderQty=size, price=entry).result()
+        if target < entry:
+            stop_limit_trigger = float(float(entry)+0.5)
+        else:
+            stop_limit_trigger = float(float(entry)-0.5)
+        client.Order.Order_new(symbol=contract, stopPx=stop_limit_trigger, price=target, execInst=str('LastPrice, ReduceOnly'), orderQty=(size*-1), ordType='StopLimit').result()
+        client.Order.Order_new(symbol=contract, stopPx=stop, execInst=str('LastPrice, ReduceOnly'), orderQty=(size*-1), ordType='Stop').result()
+    return None
+
+
+def amend_order(client, new_stop, new_target, contract_to_view):
+    if len(client.Order.Order_getOrders(symbol=contract_to_view, filter = json.dumps({'open': 'true', 'ordType': 'Stop'})).result()[0]) > 0:
+        stop = client.Order.Order_getOrders(symbol=contract_to_view, filter = json.dumps({'open': 'true', 'ordType': 'Stop'})).result()[0][0]
+    else:
+        stop = []
+    if len(client.Order.Order_getOrders(symbol=contract_to_view, filter = json.dumps({'open': 'true', 'ordType': 'Limit', 'execInst': 'Close'})).result()[0]) > 0:
+        close = client.Order.Order_getOrders(symbol=contract_to_view, filter = json.dumps({'open': 'true', 'ordType': 'Limit', 'execInst': 'Close'})).result()[0][0]
+    elif len(client.Order.Order_getOrders(symbol=contract_to_view, filter = json.dumps({'open': 'true', 'ordType': 'Limit', 'execInst': 'ParticipateDoNotInitiate,ReduceOnly'})).result()[0]) > 0:
+        close = client.Order.Order_getOrders(symbol=contract_to_view, filter = json.dumps({'open': 'true', 'ordType': 'Limit', 'execInst': 'ParticipateDoNotInitiate,ReduceOnly'})).result()[0][0]
+    elif len(client.Order.Order_getOrders(symbol=contract_to_view, filter = json.dumps({'open': 'true', 'ordType': 'Limit', 'execInst': 'ReduceOnly'})).result()[0]) > 0:
+        close = client.Order.Order_getOrders(symbol=contract_to_view, filter = json.dumps({'open': 'true', 'ordType': 'Limit', 'execInst': 'ReduceOnly'})).result()[0][0]
+    else:
+        close = []
+    qty = client.Position.Position_get(filter = json.dumps({'isOpen': True, 'symbol': contract_to_view})).result()[0][0]['currentQty']
+    orderQty = qty*-1
+    if new_stop != 0:
+        if len(stop) > 0:
+            client.Order.Order_amend(orderID=stop['orderID'], stopPx=new_stop).result()
+            print('Stop for '+contract_to_view+' Amended to '+usd_str(new_stop))
+        elif len(stop) == 0:
+            client.Order.Order_new(symbol=contract_to_view, stopPx=new_stop, execInst=str('LastPrice, ReduceOnly'), orderQty=orderQty, ordType='Stop').result()
+            print('Stop for '+contract_to_view+' Set to '+usd_str(new_stop))
+    else:
+        print('Stop Unchanged')
+    if new_target != 0 :
+        if len(close) > 0:
+            client.Order.Order_amend(orderID=close['orderID'], price=new_target).result()
+            print('Target for '+contract_to_view+' Amended to '+usd_str(new_target))
+        elif len(close) == 0:
+            client.Order.Order_new(symbol=contract_to_view, price=new_target, execInst='ReduceOnly', orderQty=orderQty, ordType='Limit').result()
+            print('Target for '+contract_to_view+' Set to '+usd_str(new_target))
+    else:
+        print('Target Unchanged')
+        
+        
+    return print('\n'+'Updated Positions'), print_dict(open_positions(client))
+
+
+def take_profit_order(client, take_profit, new_stop, new_target, contract_to_view):
+    resp = client.Position.Position_get(filter = json.dumps({'isOpen': True, 'symbol': contract_to_view})).result()[0][0]
+    take_profit_size = round(((resp['currentQty']*(int(take_profit)/100))*-1), 0)
+    client.Order.Order_cancelAll(symbol=contract_to_view).result()
+    client.Order.Order_new(symbol=contract_to_view, orderQty=take_profit_size, ordType='Market').result()
+    new_size = client.Position.Position_get(filter = json.dumps({'isOpen': True, 'symbol': contract_to_view})).result()[0][0]['currentQty']
+    if new_target != 0:
+        client.Order.Order_new(symbol=contract_to_view, price=new_target, execInst='ReduceOnly', orderQty=(new_size*-1), ordType='Limit').result()
+        print('Target for '+contract_to_view+' Set to '+usd_str(new_target))
+    if new_stop != 0:
+        client.Order.Order_new(symbol=contract_to_view, stopPx=new_stop, execInst=str('LastPrice, ReduceOnly'), orderQty=(new_size*-1), ordType='Stop').result()
+        print('Stop for '+contract_to_view+' Set to '+usd_str(new_stop))
+    return print('\n'+'Updated Positions'), print_dict(open_positions(client))
+
+
+def risk_amount_XBT(entry_value, stop_value, size):
+    risk_amount = (size*(entry_value - stop_value))
+    risk_amount = float(round(risk_amount, 8))
+    return risk_amount
+
+
+def reward_amount_XBT(entry_value, target_value, size):
+    reward_amount = (size*(target_value - entry_value))
+    reward_amount = float(round(reward_amount, 8))
+    return reward_amount
+
+
+def r_calc(reward_amount, risk_amount):
+    r_r = reward_amount/risk_amount
+    return r_r
+
+
+def position_size(entry, stop, balance, risk, target, takerFee, makerFee, order_type):
+    x = symbols('x')
+    if target > entry:
+        target_value = (1/target)+((1/target)*takerFee)
+        stop_value = (1/stop)+((1/stop)*takerFee)
+        if order_type == 'Limit':
+            entry_value = (1/entry)-((1/entry)*makerFee)
+            eq1 = Eq((x*(entry_value - stop_value)) + (balance*risk/100)) 
+        else:
+            entry_value = (1/entry)-((1/entry)*takerFee)
+            eq1 = Eq((x*(entry_value - stop_value)) + (balance*risk/100))
+    elif target < entry:
+        target_value = (1/target)-((1/target)*takerFee)
+        stop_value = (1/stop)-((1/stop)*takerFee)
+        if order_type == 'Limit':
+            entry_value = (1/entry)+((1/entry)*makerFee)
+            eq1 = Eq((x*(stop_value - entry_value)) - (balance*risk/100))
+        else:
+            entry_value = (1/entry)+((1/entry)*takerFee)
+            eq1 = Eq((x*(stop_value - entry_value)) - (balance*risk/100))
+    size = solve(eq1)
+    size = [ '%.0f' % elem for elem in size ]
+    size = size[0]
+    return size, entry_value, stop_value, target_value
+
---
+++ risk_management/risk_calculator.py
@@ -0,0 +1,283 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import MyBitmex as bm
+import MyBybit as bb
+from MyFunctions import *
+
+
+print('Begin Risk Manager')
+while True:
+    exchanges = ['Bitmex', 'Bybit', 'Exit']
+    exchange = list_user_prompt('Choose an Exchange', exchanges)
+    if exchange == 'Exit':
+        print('Exiting Script')
+        break
+    verification = check_credentials(exchange)
+    client = verification[1]
+    bot_credentials = verification[2]
+    order_types = ['Market', 'Limit']
+    valid_ticks = list(range(10))
+    percent_ticks = list(float_range(0, 100, '0.01'))[1:]+[100.00]
+    null_orders = ['Deactivated', 'Cancelled', 'Filled']
+
+    if exchange == 'Bitmex':
+        contracts = [x['symbol'] for x in client.Instrument.Instrument_getActive().result()[0] if 'XBT' in x['symbol']]
+        while True:
+            step1_options = ['View/Manage Open Positions', 'Plan New Trade', 'Change Exchange']
+            step1 = list_user_prompt('Choose Starting Option', step1_options)
+            if step1 == 'Change Exchange':
+                break
+            elif step1 == 'View/Manage Open Positions':
+                mex_positions = bm.open_positions(client)
+                active_contracts = [x['Contract'] for x in mex_positions]+['Return to Start']
+                print('\n'+'Your Bitmex Open Positions'+'\n')
+                print_dict(mex_positions)
+                step2_options = ['Close Position', 'Amend Orders', 'Take Profit', 'Return to Start']
+                step2 = list_user_prompt('Choose an Option', step2_options)
+                if step2 != 'Return to Start':
+                    contract_to_view = list_user_prompt('Choose a Position to Manage', active_contracts)
+                    if contract_to_view == 'Return to Start':
+                        print('\n')
+                        continue
+                    if step2 == 'Close Position':
+                        print('\n')
+                        bm.close_position(client, contract_to_view)
+                        continue
+                    elif step2 == 'Amend Orders':
+                        print('\n')
+                        new_stop = input_price(client, 'Enter New Stop Price or 0 to Skip', valid_ticks)
+                        new_target = input_price(client, 'Enter New Target Price or 0 to Skip', valid_ticks)
+                        bm.amend_order(client, new_stop, new_target, contract_to_view)
+                        continue
+                    elif step2 == 'Take Profit':
+                        print('\n')
+                        while True:
+                            try:
+                                take_profit = float(input('Percent of '+contract_to_view+' position to close'+'\n'+'> '))
+                            except TypeError:
+                                print('Must be a number'+'\n')
+                                continue
+                            if take_profit == 0:
+                                break
+                            else:
+                                if take_profit not in percent_ticks:
+                                    print('Invalid Entry'+'\n')
+                                    continue
+                                break
+                        if take_profit == 0:
+                            continue
+                        else:
+                            new_stop = input_price(client, 'Enter New Stop Price or 0 to Skip', valid_ticks)
+                            new_target = input_price(client, 'Enter New Target Price or 0 to Skip', valid_ticks)
+                            bm.take_profit_order(client, take_profit, new_stop, new_target, contract_to_view)
+                            continue
+
+            elif step1 == 'Plan New Trade':
+                contract = list_user_prompt('Choose Contract to Trade', contracts)
+                order_type = list_user_prompt('Choose Order Type for Entry', order_types)
+                stop = input_price(client, 'Enter Stop Price', valid_ticks)
+                target = input_price(client, 'Enter Target Price', valid_ticks)
+                contract_data = client.Instrument.Instrument_getActive().result()[0] 
+                contract_data = next(item for item in contract_data if item["symbol"] == contract)
+                bidPrice = float(contract_data['bidPrice'])
+                askPrice = float(contract_data['askPrice'])
+                makerFee = float(contract_data['makerFee'])
+                takerFee = float(contract_data['takerFee'])
+                if order_type == 'Limit':
+                    entry = input_price(client, 'Enter Entry Price', valid_ticks)
+                else:
+                    if stop > target:
+                        entry = bidPrice
+                    else:
+                        entry = askPrice
+                while True:
+                    try:
+                        risk = float(input('Account Percent Risk'+'\n'+'> '))
+                    except TypeError:
+                        print('Must be a number'+'\n')
+                        continue
+                    if risk == 0:
+                        risk = (stop - entry) / entry
+                        break
+                    else:
+                        if risk not in percent_ticks:
+                            print('Invalid Entry'+'\n')
+                            continue
+                    break
+                balance = client.User.User_getWalletHistory().result()[0][0]['walletBalance']/100000000
+                position_size_1 = bm.position_size(entry, stop, balance, risk, target, takerFee, makerFee, order_type)
+                size = int(position_size_1[0])
+                entry_value = float(position_size_1[1])
+                stop_value = float(position_size_1[2])
+                target_value = float(position_size_1[3])
+                risk_amount = bm.risk_amount_XBT(entry_value, stop_value, size)*-1
+                reward_amount = bm.reward_amount_XBT(entry_value, target_value, size)*-1
+                r_r = bm.r_calc(reward_amount, risk_amount)
+                r_r = format(r_r, '.2f')
+                new_trade = {}
+                new_trade['Contract'] = contract
+                if target < entry:
+                    new_trade['Direction'] = 'Short'
+                else:
+                    new_trade['Direction'] = 'Long'
+                new_trade['Risk Percentage'] = str(risk)+'%'
+                new_trade['Size'] = size
+                new_trade['Entry'] = usd_str(entry)
+                new_trade['Stop'] = usd_str(stop)
+                new_trade['Target'] = usd_str(target)
+                new_trade['BTC Risk'] = btc_str(risk_amount)
+                new_trade['BTC Reward'] = btc_str(reward_amount)
+                new_trade['R:R'] = r_r
+                new_trade['Starting Balance'] = str(btc_str(balance)+', '+usd_str(balance*entry))
+                new_trade['Win Balance'] = str(btc_str(balance+reward_amount)+', '+usd_str((balance+reward_amount)*target))
+                new_trade['Loss Balance'] = str(btc_str(balance-risk_amount)+', '+usd_str((balance-reward_amount)*stop))
+                print_dict(new_trade)
+                print('Execute Trade?'+'\n'+'WARNING: ALL EXISTING OPEN POSITIONS AND ORDERS FOR THIS CONTRACT WILL BE CLOSED')
+                resp = y_n_prompt()
+                if resp == 'No':
+                    print('TRADE NOT EXECUTED'+'\n')
+                    continue
+                else:
+                    if len(client.Position.Position_get(filter = json.dumps({'symbol': str(contract)})).result()[0]) != 0:
+                        if client.Position.Position_get(filter = json.dumps({'symbol': str(contract)})).result()[0][0]['currentQty'] < 0:
+                            client.Order.Order_new(symbol=contract, execInst='Close', side='Buy').result()
+                        else:
+                            client.Order.Order_new(symbol=contract, execInst='Close', side='Sell').result()
+                        bm.new_trade(client, contract, size, entry, target, stop, order_type)
+                    else:
+                        bm.new_trade(client, contract, size, entry, target, stop, order_type)
+                    print('TRADE EXECUTED')
+                    continue
+
+    elif exchange == 'Bybit':
+        contract = 'BTCUSD'
+        while True:
+            step1_options = ['View/Manage Open Positions', 'Plan New Trade', 'Change Exchange']
+            step1 = list_user_prompt('Choose Starting Option', step1_options)
+            if step1 == 'Change Exchange':
+                break
+            elif step1 == 'View/Manage Open Positions':
+                bybit_position = bb.open_positions(client, contract)
+                print('\n'+'Your Bybit Open Position'+'\n')
+                print_dict(bybit_position)
+                step2_options = ['Close Position', 'Amend Orders', 'Take Profit', 'Return to Start']
+                step2 = list_user_prompt('Choose an Option', step2_options)
+                if step2 != 'Return to Start':
+                    contract_to_view = list_user_prompt('Choose a Position to Manage', [contract, 'Return to Start'])
+                    if contract_to_view == 'Return to Start':
+                        print('\n')
+                        continue
+                    if step2 == 'Close Position':
+                        print('\n')
+                        bb.close_position(client, contract_to_view)
+                        client.Conditional.Conditional_cancelAll(symbol=contract).result()
+                        client.Order.Order_cancelAll(symbol=contract).result()
+                        continue
+                    elif step2 == 'Amend Orders':
+                        print('\n')
+                        print_dict(bb.open_positions(client, contract_to_view))
+                        new_stop = input_price(client, 'Enter New Stop Price or 0 to Skip', valid_ticks)
+                        new_target = input_price(client, 'Enter New Target Price or 0 to Skip', valid_ticks)
+                        bb.amend_order(client, new_stop, new_target, contract_to_view);
+                        print_dict(bb.open_positions(client, contract_to_view))
+                        continue
+                    elif step2 == 'Take Profit':
+                        print('\n')
+                        while True:
+                            print_dict(bb.open_positions(client, contract_to_view))
+                            try:
+                                take_profit = float(input('Percent of '+contract_to_view+' position to close'+'\n'+'> '))
+                            except TypeError:
+                                print('Must be a number'+'\n')
+                                continue
+                            if take_profit == 0:
+                                break
+                            else:
+                                if take_profit not in percent_ticks:
+                                    print('Invalid Entry'+'\n')
+                                    continue
+                                break
+                        if take_profit != 0:
+                            new_stop = input_price(client, 'Enter New Stop Price or 0 to Skip', valid_ticks)
+                            new_target = input_price(client, 'Enter New Target Price or 0 to Skip', valid_ticks)
+                            bb.take_profit_order(client, take_profit, new_stop, new_target, contract_to_view);
+                            time.sleep(1)
+                            print_dict(bb.open_positions(client, contract_to_view))
+                            continue
+
+            elif step1 == 'Plan New Trade':
+                order_type = list_user_prompt('Choose Order Type for Entry', order_types)
+                stop = input_price(client, 'Enter Stop Price', valid_ticks)
+                target = input_price(client, 'Enter Target Price', valid_ticks)
+                contract_data = client.Market.Market_symbolInfo(symbol='BTCUSD').result()[0]['result'][0]
+                bidPrice = float(contract_data['bid_price'])
+                askPrice = float(contract_data['ask_price'])
+                makerFee = [float(x['maker_fee']) for x in client.Symbol.Symbol_get().result()[0]['result'] if x['name'] == 'BTCUSD'][0]
+                takerFee = [float(x['taker_fee']) for x in client.Symbol.Symbol_get().result()[0]['result'] if x['name'] == 'BTCUSD'][0]
+                if order_type == 'Limit':
+                    entry = input_price(client, 'Enter Entry Price', valid_ticks)
+                else:
+                    if stop > target:
+                        entry = bidPrice
+                    else:
+                        entry = askPrice
+                while True:
+                    try:
+                        risk = float(input('Account Percent Risk'+'\n'+'> '))
+                    except TypeError:
+                        print('Must be a number'+'\n')
+                        continue
+                    if risk == 0:
+                        risk = (stop - entry) / entry
+                        break
+                    else:
+                        if risk not in percent_ticks:
+                            print('Invalid Entry'+'\n')
+                            continue
+                    break
+                balance = client.Wallet.Wallet_getBalance(coin='BTC').result()[0]['result']['BTC']['available_balance']
+                position_size_1 = bb.position_size(entry, stop, balance, risk, target, takerFee, makerFee, order_type)
+                size = int(position_size_1[0])
+                entry_value = float(position_size_1[1])
+                stop_value = float(position_size_1[2])
+                target_value = float(position_size_1[3])
+                risk_amount = bb.risk_amount_XBT(entry_value, stop_value, size)*-1
+                reward_amount = bb.reward_amount_XBT(entry_value, target_value, size)*-1
+                r_r = bb.r_calc(reward_amount, risk_amount)
+                r_r = format(r_r, '.2f')
+                new_trade = {}
+                new_trade['Contract'] = contract
+                if target < entry:
+                    new_trade['Direction'] = 'Short'
+                else:
+                    new_trade['Direction'] = 'Long'
+                new_trade['Risk Percentage'] = str(risk)+'%'
+                new_trade['Size'] = size
+                new_trade['Entry'] = usd_str(entry)
+                new_trade['Stop'] = usd_str(stop)
+                new_trade['Target'] = usd_str(target)
+                new_trade['BTC Risk'] = btc_str(risk_amount)
+                new_trade['BTC Reward'] = btc_str(reward_amount)
+                new_trade['R:R'] = r_r
+                new_trade['Starting Balance'] = str(btc_str(balance)+', '+usd_str(balance*entry))
+                new_trade['Win Balance'] = str(btc_str(balance+reward_amount)+', '+usd_str((balance+reward_amount)*target))
+                new_trade['Loss Balance'] = str(btc_str(balance-risk_amount)+', '+usd_str((balance-risk_amount)*stop))
+                print_dict(new_trade)
+                print('Execute Trade?'+'\n'+'WARNING: ALL EXISTING OPEN POSITIONS AND ORDERS FOR THIS CONTRACT WILL BE CLOSED')
+                resp = y_n_prompt()
+                if resp == 'No':
+                    print('TRADE NOT EXECUTED'+'\n')
+                    continue
+                else:
+                    client.Conditional.Conditional_cancelAll(symbol=contract).result()
+                    client.Order.Order_cancelAll(symbol=contract).result()
+                    if len(bb.open_positions(client, contract)) != 0:
+                        bb.close_position(client, contract)
+                    bb.new_trade(client, contract, size, entry, target, stop, order_type)
+                    print('TRADE EXECUTED')
+                    continue
+
+
+
+
