6f9ba28958e9acbbf1a481f92463b324f3afda1d
Zalzibab <zalzibab@protonmail.com>
2020-05-21 06:29:50 -0700

    Start of qTrade Tools

---
+++ qTrade/MyFunctions.py
@@ -0,0 +1,288 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from datetime import datetime, timedelta, timezone
+from sympy import symbols, Eq, solve
+from operator import itemgetter
+import pandas as pd
+import requests
+import ast
+import os
+from os import walk
+import json
+from qtrade_client.api import QtradeAPI
+from numpy import cumsum
+import io
+import time
+import decimal
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.pyplot import figure
+import matplotlib
+from matplotlib import gridspec
+import matplotlib.ticker as mtick
+
+
+def btc_float_round(value):
+    rounded_value = round(value, 8)
+    return rounded_value
+
+
+def btc_str2float_round(value):
+    rounded_value = round(float(value), 8)
+    return rounded_value
+
+
+#Legible format for BTC values
+def btc_str(value):
+    value = "{:,.8f}".format(value)
+    return value
+
+
+#Legible format for USD values
+def usd_str(value):
+    if '.' in str(value):
+        if value < 0:
+            value = value*-1
+            value = '-'+"${:,.2f}".format(value)
+        else:
+            value = "${:,.2f}".format(value)
+    else:
+        if value < 0:
+            value = value*-1
+            value = '-'+"${:,}".format(value)
+        else:
+            value = "${:,}".format(value)
+    return value
+
+
+def tz_to_timestamp(str_date):
+    new_time = datetime.timestamp(datetime.strptime(str_date, '%Y-%m-%dT%H:%M:%S.%fZ'))
+    return new_time
+
+
+def tz_to_str(tz_date, str_format):
+    str_time = datetime.strftime(datetime.strptime(tz_date, '%Y-%m-%dT%H:%M:%S.%fZ'), str_format)
+    return str_time
+
+
+def find_file(file_type):
+    f = []
+    if file_type == 'credentials':
+        file_path = str(sys.path[0])+'/credentials'
+    else:
+        file_path = str(sys.path[0])+'/configurations'
+    for (dirpath, dirnames, filenames) in walk(file_path):
+        f.extend(filenames)
+        return f
+
+
+def check_credentials(exchange):
+    while True:
+        credentials_file = [x for x in find_file('credentials') if exchange in x][0]
+        credentials = load_file(str(sys.path[0])+'/credentials/'+credentials_file)
+        print('\n')
+        if credentials['api_key'] == 'XXX':
+            print('Please Enter Your '+exchange+' API Key')
+            credentials['api_key'] = str(input('> '))
+        else:
+            print('Change Your '+exchange+' API Key?')
+            resp = y_n_prompt()
+            if resp == 'Yes':
+                print('\n')
+                print('Please Enter Your '+exchange+' API Key')
+                credentials['api_key'] = str(input('> '))
+                print('\n')
+        if credentials['bot_token'] == 'YYY' or credentials['bot_chatID'] == 'ZZZ':
+            print('\n')
+            print('Integrate Telegram Bot?')
+            resp = y_n_prompt()
+            if resp == 'Yes':
+                print('\n')
+                print('Please Enter Your Bot Token')
+                credentials['bot_token'] = str(input('> '))
+                print('\n')
+                print('Please Enter Your Telegram Chat ID')
+                credentials['bot_chatID'] = str(input('> '))
+                if telegram_sendText((credentials['bot_token'], credentials['bot_chatID']), 'Testing')['ok'] == False:
+                    print('Invalid Bot Credentials'+'\n')
+                    continue
+                else:
+                    print('\n'+'Confirm Test Message Receipt')
+                    resp = y_n_prompt()
+                    if resp == 'No':
+                        continue
+                    else:
+                        print('Bot Credentials Verified'+'\n')
+        else:
+            print('\n')
+            print('Change Your Bot?')
+            resp = y_n_prompt()
+            if resp == 'Yes':
+                print('\n')
+                print('Please Enter Your Bot Token')
+                credentials['bot_token'] = str(input('> '))
+                print('\n')
+                print('Please Enter Your Telegram Chat ID')
+                credentials['bot_chatID'] = str(input('> '))
+                if telegram_sendText((credentials['bot_token'], credentials['bot_chatID']), 'Testing')['ok'] == False:
+                    print('Invalid Bot Credentials'+'\n')
+                    continue
+                else:
+                    print('\n'+'Confirm Test Message Receipt')
+                    resp = y_n_prompt()
+                    if resp == 'No':
+                        continue
+                    else:
+                        print('Bot Credentials Verified'+'\n')
+        save_credentials = save_file(str(sys.path[0])+'/credentials/'+credentials_file, credentials)
+        try:
+            client = QtradeAPI('https://api.qtrade.io', key=credentials['api_key'])
+            client.get("/v1/user/me")
+        except:
+            print('Invalid Credentials'+'\n')
+            continue
+        break
+        save_credentials = save_file(str(sys.path[0])+'/credentials/'+credentials_file, credentials)
+        credentials_file = [x for x in find_file('credentials') if exchange in x][0]
+        credentials = load_file(str(sys.path[0])+'/credentials/'+credentials_file)
+    return print('\n'+exchange+' Credentials Verified'), client, (credentials['bot_token'], credentials['bot_chatID'])
+
+
+def load_credentials(exchange):
+    credentials_file = [x for x in find_file('credentials') if exchange in x][0]
+    credentials = load_file(str(sys.path[0])+'/credentials/'+credentials_file)
+    client = QtradeAPI('https://api.qtrade.io', key=credentials['api_key'])
+    return client, (credentials['bot_token'], credentials['bot_chatID'])
+
+
+#Telegram Text Alert
+def telegram_sendText(bot_credentials, bot_message):
+    bot_token = bot_credentials[0]
+    bot_chatID = bot_credentials[1]
+    send_text = 'https://api.telegram.org/bot'+bot_token+'/sendMessage?chat_id='+bot_chatID+'&text='+bot_message
+    response = requests.get(send_text)
+    return response.json()
+
+
+#Telegram Image Alert
+def telegram_sendImage(bot_credentials, image):
+    bot_token = bot_credentials[0]
+    bot_chatID = bot_credentials[1]
+    url = 'https://api.telegram.org/bot'+bot_token+'/sendPhoto';
+    files = {'photo': open(image, 'rb')}
+    data = {'chat_id' : bot_chatID}
+    r = requests.post(url, files=files, data=data)
+    return r
+
+
+def telegram_sendFile(bot_credentials, file):
+    bot_token = bot_credentials[0]
+    bot_chatID = bot_credentials[1]
+    url = 'https://api.telegram.org/bot'+bot_token+'/sendDocument';
+    files = {'document': open(file, 'rb')}
+    data = {'chat_id' : bot_chatID}
+    r = requests.post(url, files=files, data=data)
+    return r
+
+
+#Read .txt files
+def load_file(file):
+    temp_list = []
+    f = open(file, "r")
+    for x in f:
+        temp_list.append(x.rstrip('\n'))
+    load = [ast.literal_eval(i) for i in temp_list][0]
+    f.close()
+    return load
+
+
+def y_n_prompt():
+    while True:
+        y_n_responses = ['Yes', 'No']
+        for (x, y) in enumerate(y_n_responses):
+            print(str(x)+': '+y)
+        response = y_n_responses[int(input('> '))]
+        if response not in y_n_responses:
+            print('Invalid Selection'+'\n')
+            continue
+        else:
+            break
+    return response
+
+
+#Write to .txt file
+def save_file(file, item):
+    with open(file, mode="w") as outfile:
+        outfile.write(str(item))
+    return None
+
+
+#Sort tuples by value by index
+def sortTuple(tup, index):
+    return(sorted(tup, key = lambda x: x[index], reverse=True))
+
+
+#Remove duplicate values from sorted lists
+def sorted_list(list_to_sort):
+    return list(dict.fromkeys(list_to_sort))
+
+
+def print_dict(dict_item):
+    try:
+        for x in range(len(dict_item)):
+            for k, v in dict_item[x].items():
+                print(str(k)+': '+str(v))
+            print('\n')
+    except KeyError:
+        for k, v in dict_item.items():
+            print(str(k)+': '+str(v))
+        print('\n')
+    return None
+
+
+#User promp based list element selection by index
+def list_user_prompt(initial_dialogue, list_to_view):
+    print(initial_dialogue)
+    while True:
+        try:
+            for k, v in enumerate(list_to_view):
+                print(str(k)+': '+v)
+            resp = list_to_view[int(input('> '))]
+        except (IndexError, ValueError):
+            print('Selection must be int'+'\n')
+            continue
+        else:
+            print('Selection: '+str(resp))
+            break
+    return resp
+
+
+def create_directory(directory):
+    if os.path.exists(str(sys.path[0])+'/'+directory+'/') == False:
+        directory = directory
+        path = os.path.join(str(sys.path[0]), directory)
+        os.mkdir(path)
+    return None
+
+
+def sleep_time(count):
+    time_to_sleep = count - datetime.utcnow().minute % count
+    sleep_time = time_to_sleep*60
+    return sleep_time
+
+
+def dict_to_msg(dict_item):
+    dict_msg = ''
+    try:
+        for x in range(len(dict_item)):
+            for k, v in dict_item[x].items():
+                dict_msg += k.upper()+': '+str(v)+'\n'
+            dict_msg += '\n'
+    except KeyError:
+        for k, v in dict_item.items():
+            dict_msg += k.upper()+': '+str(v)+'\n'
+        dict_msg += '\n'
+    return dict_msg
+
---
+++ qTrade/trade_history.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#Import libraries
+from MyFunctions import *
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, filename=sys.path[0]+'/trade_history.log', format='%(asctime)s :: %(levelname)s :: %(message)s')
+import warnings
+warnings.simplefilter("ignore")
+
+
+exchange = 'qTrade'
+verification = check_credentials(exchange)
+create_directory('reports')
+csv_directory = str(sys.path[0])+'/reports/'
+while True:
+    try:
+        refresh_time = int(input('Number of minutes between order updates'+'\n'+'> '))
+    except TypeError:
+        print('Must be a whole number'+'\n')
+        continue
+    break
+
+
+while True:
+    msg = ''
+    date = datetime.utcnow().strftime('%m-%d-%Y %H:%M:%S')
+    verification = load_credentials(exchange);
+    logger.info('Loop Started')
+    client = verification[0]
+    bot_credentials = verification[1]
+
+    if os.path.exists('last_trade_recorded'):
+        last_trade_recorded = float(load_file('last_trade_recorded'))
+        logger.info('Last Trade Timestamp: '+str(last_trade_recorded))
+    else:
+        last_trade_recorded = 0
+        logger.info('No Trade History On Record')
+    #Fetch Execution History
+    execution_history = client.get('/v1/user/trades')['trades']
+    execution_history = sorted(execution_history, key=itemgetter('created_at'))
+    newer_than = execution_history[-1]['order_id']
+    pull = True
+    while pull:
+        temp_data = client.get('/v1/user/trades', newer_than=newer_than)['trades']
+        if len(temp_data) == 0:
+            pull = False
+        else:
+            execution_history.append(temp_data)
+            execution_history = sorted(execution_history, key=itemgetter('created_at'))
+            newer_than = execution_history[-1]['order_id']
+
+    last_executed = tz_to_timestamp(execution_history[-1]['created_at'])
+    updated_last_trade = save_file('last_trade_recorded', last_executed);
+    
+    if last_trade_recorded != last_executed:
+        logger.info('New Trades Detected')
+        #Get list of all market_strings with execution history
+        markets_traded = [x['market_string'] for x in execution_history]
+        markets_traded.sort()
+        markets_traded = sorted_list(markets_traded)
+
+        #Parse Execution History by market and sort by timestamp
+        coin_specific_trades = {}
+        for x in markets_traded:
+            coin_specific_trades[x] = [y for y in execution_history if y['market_string'] == x]
+            coin_specific_trades[x] = sorted(coin_specific_trades[x], key=itemgetter('created_at'))
+
+        #Create DataFrames for each coin traded
+        coin_specific_dfs = []
+        for x in range(len(coin_specific_trades)):
+            coin_specific_dfs.append(pd.DataFrame(data=coin_specific_trades[markets_traded[x]], index=list(range(len(coin_specific_trades[markets_traded[x]])))))
+
+        #Reformat DataFrames and save to csv
+        drops = ['id', 'order_id', 'market_id']
+        for x in range(len(coin_specific_dfs)):
+            coin_specific_dfs[x] = coin_specific_dfs[x].drop(columns=drops)
+            for y in range(len(coin_specific_dfs[x])):
+                coin_specific_dfs[x]['created_at'].iloc[y] = datetime.strftime(datetime.strptime(coin_specific_dfs[x]['created_at'].iloc[y], '%Y-%m-%dT%H:%M:%S.%fZ'), '%m-%d-%Y %H:%M')
+            columns = list(coin_specific_dfs[x].columns)
+            columns = columns[-1:] + columns[:-1]
+            coin_specific_dfs[x] = coin_specific_dfs[x][columns]
+            coin_specific_dfs[x].to_csv(csv_directory+markets_traded[x]+'_trades.csv', index=False)
+
+        new_executions = {}
+        for x in markets_traded:
+            new_executions[x] = [y for y in execution_history if y['market_string'] == x]
+            new_executions[x] = [l for l in new_executions[x] if tz_to_timestamp(l['created_at']) > last_trade_recorded]
+            new_executions[x] = sorted(new_executions[x], key=itemgetter('created_at'))
+            if len(new_executions[x]) == 0:
+                del new_executions[x]
+
+        new_markets = list(new_executions.keys())      
+        all_new_trades= []
+        for x in new_markets:
+            for y in range(len(new_executions[x])):
+                clean_dict = {}
+                clean_dict['Market'] = new_executions[x][y]['market_string']
+                clean_dict['Timestamp'] = tz_to_str(new_executions[x][y]['created_at'], '%m-%d-%Y %H:%M')
+                clean_dict['Side'] = new_executions[x][y]['side'].upper()
+                clean_dict['Quote'] = new_executions[x][y]['market_amount']
+                clean_dict['Price'] = new_executions[x][y]['price']
+                clean_dict['Base'] = new_executions[x][y]['base_amount']
+                all_new_trades.append(clean_dict)
+        if bot_credentials is not None:
+            msg = 'Updated Trades'+'\n'+date+'\n'+'\n'      
+            msg += dict_to_msg(all_new_trades)
+            
+            open_orders = client.orders(open=True)
+            wallet_balances = {}
+            balances = client.balances()
+            for k, v in balances.items():
+                if float(balances[k]) > 0:
+                    wallet_balances[k] = float(balances[k])
+
+            for k, v in wallet_balances.items():
+                wallet_balances[k] += sum([float(x['market_amount_remaining']) for x in open_orders if x['order_type'] == 'sell_limit' and x['market_string'][:-4] == k])
+            wallet_balances['BTC'] += sum([float(x['base_amount']) for x in open_orders if x['order_type'] == 'buy_limit'])
+            
+            msg += '\n'+'Final Balances'+'\n'
+            msg += dict_to_msg(wallet_balances)
+            telegram_sendText(bot_credentials, msg);
+            logger.info('Msg Sent')
+            time.sleep(sleep_time(refresh_time))
+    else:
+        logger.info('Loop Completed')
+        time.sleep(sleep_time(refresh_time))
+
